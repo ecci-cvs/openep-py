b1277b7ae0443e7e7ea937f7ae1fea53f1f01e91
Vinush Vigneswaran <150042692+vinush-vignes@users.noreply.github.com>
2025-03-24 20:13:43 +0000

    Feature/SM-107: Add multiple ablation points (#16)
    * find_mesh_points_at_distance from points of interest
    
    * add multiple ablation points in one go
    
    * Feature/SM-106: Histogram to region (#17)
    
    * added internal names and names to ablation object
    
    * points should be ablation_points
    
    * Feature/SM-125: Fibre (LON) data to openep format (#18)
    
    * if points data is empty for boundaries, then skip
    
    * allows opencarp data -> openep data to be loaded when egm is empty
    
    * adding vector data to openep export of surface data
    
    * move fibre load to extract_vector_data function
    
    * Bugfix/SM-131: Unable to select landmarks for mesh registration (#20)
    
    * setting pyvista version, due to vtk conflict in github actions
    
    * bugfix when loading with no linear connections
    
    * selecting landmarks for mesh registration
    
    * Feature/SM-135: Work-In-Progress WIPs (#21)
    
    * slight smoothing of mesh to avoid freeze up of repair function
    
    * remove repair kwargs
    
    * deal with 1 in lon file
    
    * ablation points copy corrected
    
    * bug-fix load linear_connection_regions
    
    * bug-fix setter function and custom field extract
    
    * added test_additional_fields_export and test_opencarp_mat_export tests
    
    * dynamically write all custom fields to openep
    
    * read all custom fields dynamically
    
    * copy vectors too when copying Case
    
    * read fibres from old version and new version (output only new version)
    
    * Feature/SM-36: Conduction Velocity with RBF Method (#22)
    
    * added rbf to analyser UI options and cv methods
    
    * Feature/SM-144: Simulation Refactor (#24)
    
    * load vtk with fibres the length of cells not points
    
    * load simulation mid run
    
    * fixed number of fibres export
    
    * exclude cv field according to thresholds UI and backend
    
    * create ablation points when there is none (#25)
    
    * Feature/SM-140: WIP, Simulations and builds on Windows (#23)
    
    * remove all import of vedo
    
    * convert np.Nan to np.nan for newer numpy

--- openep/analysis/analyse.py
+++ openep/analysis/analyse.py
@@ -131,7 +131,7 @@ class ConductionVelocity:
                - self._case.electric.annotations.reference_activation_time[include])
 
         cv_method = supported_cv_methods[method]
-        self.values, self.centers = cv_method(bipolar_egm_pts, lat, **method_kwargs)
+        self.values, self.centers = cv_method(bipolar_egm_pts, lat, self._case, **method_kwargs)
 
         if apply_scalar_field:
             self._case.fields.conduction_velocity = interpolate_general_cloud_points_onto_surface(
@@ -225,3 +225,43 @@ class Divergence:
             self._case.fields.cv_divergence = self.values
 
         return self.direction, self.values
+
+    def exclude_collision_points(
+            self,
+            cv_field,
+            divergence_field,
+            collision_threshold=-3,
+            focal_threshold=3,
+            radius=4,
+    ):
+        """
+        Exclude conduction velocity values near regions of wave collision or focal discharge.
+
+        Args
+            cv_field : np.ndarray
+                Array of conduction velocity values corresponding to each mesh point.
+            divergence_field : np.ndarray
+                Array of divergence values at each mesh point.
+            collision_threshold : float, optional
+                Threshold for detecting wave collisions (default is -3).
+            focal_threshold : float, optional
+                Threshold for detecting focal discharges (default is 3).
+            radius : float, optional
+                Neighborhood radius within which to check for collision conditions (default is 4).
+
+        Returns
+            np.ndarray
+                Array of conduction velocity values, with values replaced by np.nan where a collision is detected.
+        """
+        temp_mesh = self._case.create_mesh()
+
+        cv_field = exclude_collision_points(
+            mesh=temp_mesh,
+            cv_field=cv_field,
+            divergence_field=divergence_field,
+            collision_threshold=collision_threshold,
+            focal_threshold=focal_threshold,
+            radius=radius,
+        )
+
+        return cv_field
